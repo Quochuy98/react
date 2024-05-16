04fdde3fbfa3f71bd20a321de553e8fedb7af912
Quoc Huy Tran <huytq@payme.vn>
2024-05-16 10:23:48 +0700

    ádsad

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  console.log('🚀 ~ App ~ apiUrl:', apiUrl)
+  const apiKey = process.env.REACT_APP_API_KEY;
+  console.log('🚀 ~ App ~ apiKey:', apiKey)
   return (
     <div className="App">
       <header className="App-header">
+        <h2>{process.env.TITLE_NAME}</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
